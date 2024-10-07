8cd93ff3ae2fee08e28e62aae1b5418346416215
Pheanouk <165833744+PheanoukHun@users.noreply.github.com>
2024-10-07 16:21:03 +0000

    Standardizing Formating Part 2

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ import math
 
 ## Iniatilization of the Variables
 
-data_list = None;
+data_list = [];
 standard_deviation = 0;
 standard_uncertainty = 0;
 sum_of_differences = 0;
-n = None;
+n = 0;
 mean = 0;
 low = 0;
 high = 0;
+std_squared = 0;
 
 # Taking the Data
 with open("dataList.txt") as file:
-    data_list = file.read().split(",")
-    n = len(data_list)
+    data_list = file.read().split(",");
+    n = len(data_list);
 
 # Converts all Data Points into a Float
 for i in range(len(data_list)):
-    data_list[i] = float(data_list[i])
+    data_list[i] = float(data_list[i]);
 
 ## Finding the Mean for Later Use
 
@@ -29,30 +30,30 @@ for data in data_list:
     mean += data;
 
 # Find Mean
-mean = mean / n
+mean = mean / n;
 
 ## Finding the Standard Deviation
 
 # Summation of the differences between data point and mean
 for data in data_list:
-    sum_of_differences += ((data - mean) ** 2)
+    sum_of_differences += ((data - mean) ** 2);
 
 # Finding Standard Deviation
-temp = (1/(n-1)) * sum_of_differences
-standard_deviation = math.sqrt(temp)
+std_squared = (1/(n-1)) * sum_of_differences;
+standard_deviation = math.sqrt(std_squared);
 
 ## Finding Standard Uncertainty
-standard_uncertainty = standard_deviation / math.sqrt(n)
+standard_uncertainty = standard_deviation / math.sqrt(n);
 
 ## Minimum and Maximum Confidence Interval
-low = mean - (2 * standard_uncertainty)
-high = mean + (2 * standard_uncertainty)
+low = mean - (2 * standard_uncertainty);
+high = mean + (2 * standard_uncertainty);
 
 ## Print Out Results
-print("\nData Points: ", end = "")
-print(*data_list, sep = ", ")
-print(f"Mean: {round(mean, 10)}")
-print(f"Standard Deviation: {round(standard_deviation, 10)}")
-print(f"Standard Uncertainty: {round(standard_uncertainty, 10)}")
-print(f"Confidence Interval:\n   -Lower Internal: {low}\n   -Higher Interval: {high}")
-print()
+print("\nData Points: ", end = "");
+print(*data_list, sep = ", ");
+print(f"Mean: {round(mean, 10)}");
+print(f"Standard Deviation: {round(standard_deviation, 10)}");
+print(f"Standard Uncertainty: {round(standard_uncertainty, 10)}");
+print(f"Confidence Interval:\n   -Lower Internal: {low}\n   -Higher Interval: {high}");
+print();
