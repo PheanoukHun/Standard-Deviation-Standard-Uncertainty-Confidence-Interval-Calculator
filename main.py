378f0a5f23f843eda10d7ed74c4271169f6846c0
Pheanouk <165833744+PheanoukHun@users.noreply.github.com>
2024-10-07 16:06:47 +0000

    Outchanges and Internal Changes for Efficiency

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ with open("dataList.txt") as file:
     data_list = file.read().split(",")
     n = len(data_list)
 
+# Converts all Data Points into a Float
+for i in range(len(data_list)):
+    data_list[i] = float(data_list[i])
+
 ## Finding the Mean for Later Use
 
 # Sum of All data
 for data in data_list:
-    mean += float(data);
+    mean += data;
 
 # Find Mean
 mean = mean / n
@@ -26,9 +30,9 @@ mean = mean / n
 # Summation of the differences between data point and mean
 sum_of_differences = 0
 for data in data_list:
-    sum_of_differences += ((float(data) - mean) ** 2)
+    sum_of_differences += ((data - mean) ** 2)
 
-# Finding Stand Deviation
+# Finding Standard Deviation
 temp = (1/(n-1)) * sum_of_differences
 standard_deviation = math.sqrt(temp)
 
@@ -36,11 +40,14 @@ standard_deviation = math.sqrt(temp)
 standard_uncertainty = standard_deviation / math.sqrt(n)
 
 ## Minimum and Maximum Confidence Interval
-min = standard_deviation - 2 * standard_uncertainty
-max = standard_deviation + 2 * standard_uncertainty
+low = standard_deviation - (2 * standard_uncertainty)
+high = standard_deviation + (2 * standard_uncertainty)
 
 ## Print Out Results
-print(f"\nMean: {round(mean, 10)}")
+
+print(f"\nData Points: {data_list}")
+print(f"Mean: {round(mean, 10)}")
 print(f"Standard Deviation: {round(standard_deviation, 10)}")
 print(f"Standard Uncertainty: {round(standard_uncertainty, 10)}")
-print(f"Confidence Interval:\nLower Internal: {min}\nHigher Interval: {max}")
+print(f"Confidence Interval:\n   -Lower Internal: {low}\n   -Higher Interval: {high}")
+print()
